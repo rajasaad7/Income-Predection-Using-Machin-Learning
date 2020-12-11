c5bf576105acbffc58d6f7ce505cec84659544d9
Raja Muhammad Saad <36265727+rajasaad7@users.noreply.github.com>
2020-12-11 22:34:07 +0400

    Add files via upload

---
+++ Income-Prediction.py
@@ -0,0 +1,195 @@
+
+###### Step 1: Load Libaraies
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from collections import Counter
+
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, VotingClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, learning_curve, train_test_split, KFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+sns.set(style='white', context='notebook', palette='deep')
+
+######3 Step 2: Load Data
+
+
+dataset = pd.read_csv("data.csv")
+
+# Check for Null Data
+dataset.isnull().sum()
+
+
+
+# Replace All Null Data in NaN
+dataset = dataset.fillna(np.nan)
+
+
+# Get data types
+print(dataset.dtypes)
+
+# Peek at data
+dataset.head(4)
+
+# Reformat Column We Are Predicting
+dataset['income']=dataset['income'].map({'<=50K': 0, '>50K': 1, '<=50K.': 0, '>50K.': 1})
+print(dataset.head(4))
+
+
+#######3 Step 3: Analyze Data
+
+# Identify Numeric features
+numeric_features = ['age','fnlwgt','education.num','capital.gain','capital.loss','hours.per.week','income']
+
+# Identify Categorical features
+cat_features = ['workclass','education','marital.status', 'occupation', 'relationship', 'race', 'sex', 'native']
+
+# Count of >50K & <=50K
+sns.countplot(dataset['income'],label="Count")
+plt.show()
+
+# Correlation matrix between numerical values
+g = sns.heatmap(dataset[numeric_features].corr(),annot=True, fmt = ".2f", cmap = "coolwarm")
+plt.show()
+
+# Explore Education Num vs Income
+g = sns.factorplot(x="education.num",y="income",data=dataset,kind="bar",size = 6,palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels(">50K probability")
+
+# Explore Hours Per Week vs Income
+g  = sns.factorplot(x="hours.per.week",y="income",data=dataset,kind="bar",size = 6,palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels(">50K probability")
+
+# Explore Age vs Income
+g = sns.FacetGrid(dataset, col='income')
+g = g.map(sns.distplot, "age")
+plt.show()
+
+# Fill Missing Category Entries
+dataset["workclass"] = dataset["workclass"].fillna("X")
+dataset["occupation"] = dataset["occupation"].fillna("X")
+dataset["native.country"] = dataset["native.country"].fillna("United-States")
+
+# Confirm All Missing Data is Handled
+dataset.isnull().sum()
+
+# Fill Missing Category Entries
+dataset["workclass"] = dataset["workclass"].fillna("X")
+dataset["occupation"] = dataset["occupation"].fillna("X")
+dataset["native.country"] = dataset["native.country"].fillna("United-States")
+
+# Confirm All Missing Data is Handled
+dataset.isnull().sum()
+
+# Explore Sex vs Income
+g = sns.barplot(x="sex",y="income",data=dataset)
+g = g.set_ylabel("Income >50K Probability")
+plt.show()
+
+# Explore Relationship vs Income
+g = sns.factorplot(x="relationship",y="income",data=dataset,kind="bar", size = 6 ,
+palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels("Income >50K Probability")
+plt.show()
+
+# Explore Marital Status vs Income
+g = sns.factorplot(x="marital.status",y="income",data=dataset,kind="bar", size = 6 ,
+palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels("Income >50K Probability")
+plt.show()
+
+# Explore Workclass vs Income
+g = sns.factorplot(x="workclass",y="income",data=dataset,kind="bar", size = 6 ,
+palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels("Income >50K Probability")
+plt.show()
+
+##### Step 4 : Feature Engineering
+
+# Convert Sex value to 0 and 1
+dataset["sex"] = dataset["sex"].map({"Male": 0, "Female":1})
+
+# Create Married Column - Binary Yes(1) or No(0)
+dataset["marital.status"] = dataset["marital.status"].replace(['Never-married','Divorced','Separated','Widowed'], 'Single')
+dataset["marital.status"] = dataset["marital.status"].replace(['Married-civ-spouse','Married-spouse-absent','Married-AF-spouse'], 'Married')
+dataset["marital.status"] = dataset["marital.status"].map({"Married":1, "Single":0})
+dataset["marital.status"] = dataset["marital.status"].astype(int)
+
+# Drop the data you don't want to use
+dataset.drop(labels=["workclass","education","occupation","relationship","race","native.country"], axis = 1, inplace = True)
+print('Dataset with Dropped Labels')
+print(dataset.head())
+
+##### Step 5 : Modeling
+
+# Split-out Validation Dataset and Create Test Variables
+array = dataset.values
+X = array[:,0:8]
+Y = array[:,8]
+print('Split Data: X')
+print(X)
+print('Split Data: Y')
+print(Y)
+validation_size = 0.20
+seed = 7
+num_folds = 10
+scoring = 'accuracy'
+X_train, X_validation, Y_train, Y_validation = train_test_split(X,Y,
+    test_size=validation_size,random_state=seed)
+
+# Params for Random Forest
+num_trees = 100
+max_features = 3
+
+#Spot Check 5 Algorithms (LR, LDA, KNN, CART, GNB, SVM)
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('RF', RandomForestClassifier(n_estimators=num_trees, max_features=max_features)))
+#models.append(('SVM', SVC()))
+# evalutate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = plt.figure()
+fig.suptitle('Algorith Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+##### Step 6 : Finalize Model
+
+# a) Predictions on validation dataset - KNN
+random_forest = RandomForestClassifier(n_estimators=250,max_features=5)
+random_forest.fit(X_train, Y_train)
+predictions = random_forest.predict(X_validation)
+print("Accuracy: %s%%" % (100*accuracy_score(Y_validation, predictions)))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
